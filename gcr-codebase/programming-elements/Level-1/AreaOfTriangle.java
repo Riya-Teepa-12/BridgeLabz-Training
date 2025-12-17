2a3d1782203ad0e34adb5ef6d489306927978dee
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-17 23:26:29 +0530

    [Riya] : Added for calculating area of triangle

---
+++ gcr-codebase/programming-elements/Level-1/AreaOfTriangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class AreaOfTriangle{
+  public static void main(String []args){
+   	Scanner kb=new Scanner(System.in);
+       // Taking height and base from the user as input
+        double height= kb.nextDouble();
+        double base= kb.nextDouble();
+      
+      //calculating area of triangle in cm, feet , inches
+        double areaTriangleInCm = 0.5 * height * base;
+        double areaTriangleInInInches = areaTriangleInCm/2.54;
+        double areaTriangleInFeet =  areaTriangleInInInches/12;
+     
+     //Displaying area of a triangle in square inches and square centimeters 
+        System.out.println("Your Area in cm is"+ areaTriangleInCm +"while in feet is"+areaTriangleInFeet +"and inches is"+ areaTriangleInInInches);
+   } 
+}
