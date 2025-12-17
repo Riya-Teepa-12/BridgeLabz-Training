ca6719012d122fa9ffd42919e9d9dd8d512b40a5
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-17 23:24:58 +0530

    [Riya] : Added for Finding Age

---
+++ gcr-codebase/programming-elements/Level-1/FindAge.java
@@ -0,0 +1,12 @@
+class FindAge{
+   public static void main(String [] args){
+     // Birth year of Harry
+     int HarryBirthYear= 2000; 
+     // Current year
+     int CurrentYear= 2024;
+     // Calculate age by subtracting birth year from current year
+     int HarryAge=CurrentYear-HarryBirthYear;
+     // Print Harry's age
+     System.out.println("Harry's age in 2024 is"+  HarryAge);
+}
+}
