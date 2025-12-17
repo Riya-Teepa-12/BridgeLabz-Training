5b7474be4a8218f9a56127688f49be628a1c7c52
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-17 23:25:47 +0530

    [Riya] : Added for calculating average marks

---
+++ gcr-codebase/programming-elements/Level-1/AverageMark.java
@@ -0,0 +1,14 @@
+class AverageMark{
+  public static void main(String [] args){
+      // Sam's marks in maths
+      int MathMark=94;
+      // Sam's marks in Physics
+      int PhysicsMark=95;
+      // Sam's marks in Chemistry
+      int ChemistryMark=96;
+     // The Average Marks of Sam's in PCM 
+      int Average=((MathMark+PhysicsMark+ChemistryMark)/3);
+      System.out.println("Sam's average mark in PCM is" + Average);
+     
+  }
+}
