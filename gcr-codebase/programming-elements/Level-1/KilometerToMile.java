a8c06cde52b57bdee38ce45914513d3f635dbcce
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-17 23:30:33 +0530

    [Riya] : Added for converting kilometer to mile

---
+++ gcr-codebase/programming-elements/Level-1/KilometerToMile.java
@@ -0,0 +1,11 @@
+class KilometerToMile{
+    public static void main(String []args){
+       //Distance of 10.8 kilometers
+       double distance=10.8;
+       // 1km=1.6miles
+       //Converting kilometers to miles
+       System.out.println("The distance"+ distance+"km in miles is" + distance*1.6);
+        
+     
+ }
+}
