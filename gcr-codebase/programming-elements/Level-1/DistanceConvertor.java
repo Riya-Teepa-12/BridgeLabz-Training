a7b38db7ab8f0c6fd55c9e0e7ecdc6623c01a8b7
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-17 23:56:56 +0530

    [Riya] : Added for converting distance

---
+++ gcr-codebase/programming-elements/Level-1/DistanceConvertor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class DistanceConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input distance in feet
+        System.out.print("Enter distance in feet: ");
+        double distanceInFeet = sc.nextDouble();
+
+        // Conversions
+        double distanceInYards = distanceInFeet / 3;
+        double distanceInMiles = distanceInYards / 1760;
+
+        // displaying the distance in yards and feet
+        System.out.println("The distance in yards is " + distanceInYards +
+                           " while the distance in miles is " + distanceInMiles);
+    }
+}
