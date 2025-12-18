68a3a9bc19f9ce5a770e2e28fc491a9765263c1f
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-18 12:09:02 +0530

    [Riya] : Added for checking divisibility of 5

---
+++ gcr-codebase/control-flow/Level-1/CheckDivisibility.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class CheckDivisibility{
+   public static void main(String []args){
+       //to check if a number is divisible by 5
+     
+       Scanner kb=new Scanner(System.in);
+       //taking number as input from the user
+       int number=kb.nextInt();
+       //storing result
+       Boolean flag=false;
+       //checking if the number is divisible by 5 or not
+       if(number%5==0){
+          flag=true;
+         } 
+       //displaying the output
+      System.out.println("Is the number" + " " +number+" " + "divisible by 5?" +" " +flag+ " ");
+     }
+}
