67978c5f85029c5ca9e7a5a93e49a1ff4772004f
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-18 13:06:41 +0530

    [Riya] : Added for checking sum of n natural numbers

---
+++ gcr-codebase/control-flow/Level-1/SumOfNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfNumbers{
+    public static void main(String []args){
+           //creating object of Scanner and taking number as input from user
+     Scanner input=new Scanner(System.in);
+           int n=input.nextInt();
+          
+          //the sum of n natural numbers 
+          int sumOfNumber= (n * (n+1))/2;
+
+          if(n<0){
+            System.out.println("The number" +" "+ n +" " + "is not a natural number");
+           }
+            else
+              System.out.println("The sum of"+" " +n+ " "+"natural numbers is" +" " +sumOfNumber);
+         
+    }
+}
