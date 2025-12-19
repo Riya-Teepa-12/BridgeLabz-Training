c177e120d6c09da9ee56bd77b5a3d32b50d541fa
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-19 12:51:16 +0530

    [Riya] : Added for printing the table of given number

---
+++ gcr-codebase/arrays/Level-01/MultiplicationTable.java
@@ -0,0 +1,31 @@
+// Create  MultiplicationTable Class to compute the table of any number using 
+// an array
+import java.util.Scanner;
+
+class MultiplicationTable{
+   public static void main(String[] args) {
+      // Create a Scanner Object
+      Scanner input = new Scanner(System.in);
+
+      // creating a variable and taking integer  input from the user
+         System.out.println("Enter a number: ");
+        int number= input.nextInt();
+
+       //defining a integer array 
+        int arr[] =new int[10];
+       
+       //taking input from the user
+         for(int i=1;i<arr.length;i++){
+            arr[i]=(i) * number;
+        }
+
+       //displaying the table
+           for(int i=1;i<arr.length;i++){
+             System.out.println("the result from the array in the " +  number +"*" + i+ "="+arr[i] );
+           }
+         
+            input.close();
+            System.exit(0);
+      }
+}
+
