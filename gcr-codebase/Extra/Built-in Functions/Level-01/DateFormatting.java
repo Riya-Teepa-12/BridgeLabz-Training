2250e343a6c57aafa25d7363ab3e5b82d736e2dc
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-24 12:28:30 +0530

    [Riya] : Added for formatting of dates

---
+++ gcr-codebase/Extra/Built-in Functions/Level-01/DateFormatting.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatting{
+
+	public static void main(String[] args) {
+
+       //creating object of LocalDate
+		LocalDate today=LocalDate.now();
+
+		DateTimeFormatter f1=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter f2=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		DateTimeFormatter f3=DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+
+		System.out.println("Format dd/MM/yyyy : "+today.format(f1));
+		System.out.println("Format yyyy-MM-dd : "+today.format(f2));
+		System.out.println("Format EEE, MMM dd, yyyy : "+today.format(f3));
+	}
+}
