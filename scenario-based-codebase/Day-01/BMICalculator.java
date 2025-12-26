d8f758c44525d980455aab7cf8063a561de28e3d
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-26 11:02:50 +0530

    [Riya] : Added for calculating BMI

---
+++ scenario-based-codebase/Day-01/BMICalculator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class BMICalculator{
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+	//taking integer input as weight and height for calculating BMI
+       System.out.print("enter weight in kg: ");
+        double weight= input.nextDouble();
+        System.out.print("enter height : ");
+        double height= input.nextDouble();
+	   
+        //calculating BMI
+        double bmi= weight/(height*height);
+
+        System.out.println("BMI= " + bmi);
+         //displaying the result according to the bmi
+	     if(bmi<=18.4)
+          System.out.println("Status: Underweight");
+         else if(bmi>=18.5 && bmi<=24.9)
+           System.out.println("Status: Normal weight");
+          else if(bmi>=25 && bmi<=39.9)
+            System.out.println("Status: Overweight");
+
+        input.close();
+    }
+}
