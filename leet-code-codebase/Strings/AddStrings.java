3a31d18e471d1240efeeeccce0181cf20ebf030f
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-28 23:22:56 +0530

    [Riya] : added for adding strings

---
+++ leet-code-codebase/Strings/AddStrings.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class AddStrings{
+
+    // Method to add two numeric strings
+    public static String addStrings(String num1, String num2) {
+
+        int min = Math.min(num1.length(), num2.length());
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+
+        // Add digits from right to left for common length
+        for (int i = 0; i < min; i++) {
+            int a = Character.getNumericValue(num1.charAt(num1.length() - i - 1));
+            int b = Character.getNumericValue(num2.charAt(num2.length() - i - 1));
+
+            sb.append((a + b + carry) % 10);
+            carry = (a + b + carry) / 10;
+        }
+
+        if (num1.length() > num2.length()) {
+            for (int i = num1.length() - min - 1; i >= 0; i--) {
+                int a = Character.getNumericValue(num1.charAt(i));
+                sb.append((a + carry) % 10);
+                carry = (a + carry) / 10;
+            }
+        }
+        else {
+            for (int i = num2.length() - min - 1; i >= 0; i--) {
+                int a = Character.getNumericValue(num2.charAt(i));
+                sb.append((a + carry) % 10);
+                carry = (a + carry) / 10;
+            }
+        }
+
+       
+        if (carry != 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+    }
+
+    // Main method to take input from user
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        String num1 = input.nextLine();
+
+        System.out.print("Enter second number: ");
+        String num2 = input.nextLine();
+
+        // Call addStrings method
+        String result = addStrings(num1, num2);
+        System.out.println("Sum = " + result);
+
+        input.close();
+    }
+}
