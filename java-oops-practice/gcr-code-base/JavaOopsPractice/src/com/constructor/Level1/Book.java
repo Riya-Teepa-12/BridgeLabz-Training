2d0fb7cece364db04af8ff5b4150ee40f2da5c44
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-28 16:54:03 +0530

    [Riya] : added for checking book details

---
+++ java-oops-practice/gcr-code-base/JavaOopsPractice/src/com/constructor/Level1/Book.java
@@ -0,0 +1,33 @@
+package com.constructor.Level1;
+
+public class Book {
+	
+	
+
+	private String title;
+	private String author;
+	private double price;
+	
+	//constructor
+	public Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+	// Parameterized constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    
+    // Method to display book details
+    public void displayDetails() {
+        System.out.println("Title  : " + title);
+        System.out.println("Author : " + author);
+        System.out.println("Price  : ₹" + price);
+
+    }
+
+
+}
