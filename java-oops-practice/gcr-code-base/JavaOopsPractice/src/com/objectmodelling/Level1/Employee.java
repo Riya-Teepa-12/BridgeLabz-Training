1bd772895e906b8d6c94b652fa46471b833743b5
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-31 22:42:48 +0530

    [Riya] : added for the employee details

---
+++ java-oops-practice/gcr-code-base/JavaOopsPractice/src/com/objectmodelling/Level1/Employee.java
@@ -0,0 +1,13 @@
+package com.objectmodelling.Level1;
+
+public class Employee {
+	 private String name;
+
+	    Employee(String name) {
+	        this.name = name;
+	    }
+
+	    void showEmployee() {
+	        System.out.println("   Employee: " + name);
+	    }
+}
