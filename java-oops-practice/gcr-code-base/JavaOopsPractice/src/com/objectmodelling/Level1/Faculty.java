8ea66bde33bddf2dbc16fa988cf0e0ee1b5348d7
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-31 22:43:19 +0530

    [Riya] : added for the faculty bdetails

---
+++ java-oops-practice/gcr-code-base/JavaOopsPractice/src/com/objectmodelling/Level1/Faculty.java
@@ -0,0 +1,13 @@
+package com.objectmodelling.Level1;
+
+public class Faculty {
+	 private String name;
+
+	    Faculty(String name) {
+	        this.name = name;
+	    }
+
+	    void showFaculty() {
+	        System.out.println("Faculty: " + name);
+	    }
+}
