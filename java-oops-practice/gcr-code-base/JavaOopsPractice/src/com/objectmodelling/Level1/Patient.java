92498fbaff5f475eb5758551fb6aec3631ba9a08
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2025-12-31 22:44:55 +0530

    [Riya] : added for the patient details

---
+++ java-oops-practice/gcr-code-base/JavaOopsPractice/src/com/objectmodelling/Level1/Patient.java
@@ -0,0 +1,17 @@
+package com.objectmodelling.Level1;
+
+public class Patient {
+	 
+          //attributes
+	 private String name;
+           
+              //constructor
+	    public Patient(String name) {
+	        this.name = name;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+     
+}
