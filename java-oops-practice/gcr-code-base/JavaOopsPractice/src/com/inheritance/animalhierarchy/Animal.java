8b24e6ca43346b4a0ac556dd467659fcb1b2db81
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-01 11:45:10 +0530

    [Riya] : added for the animal details

---
+++ java-oops-practice/gcr-code-base/JavaOopsPractice/src/com/inheritance/animalhierarchy/Animal.java
@@ -0,0 +1,19 @@
+package com.inheritance.animalhierarchy;
+
+public class Animal {
+	private String name;
+	private int age;
+	
+	
+	public Animal(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+
+	public void makeSound() {
+		System.out.println("Animal make different  sound");
+	}
+	
+
+}
