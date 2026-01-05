548c40a35b9a9e7ab1fc6537b34807a524f55a7f
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-05 11:21:43 +0530

    [Riya] : added for the two sum problem

---
+++ dsa-practice/gcr-code-base/DSA/src/com/hashmapandfunction/TwoSumProblem.java
@@ -0,0 +1,32 @@
+package com.hashmapandfunction;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumProblem {
+	  public static int[] twoSum(int[] nums, int target) {
+	        Map<Integer, Integer> map = new HashMap<>();
+
+	        for (int i = 0; i < nums.length; i++) {
+	            int required = target - nums[i];
+
+	            if (map.containsKey(required)) {
+	                return new int[]{map.get(required), i};
+	            }
+
+	            map.put(nums[i], i);
+	        }
+	        return new int[]{-1, -1};
+	    }
+
+	    // Main method
+	    public static void main(String[] args) {
+	        int[] nums = {2, 7, 11, 15};
+	        int target = 9;
+
+	        int[] result = twoSum(nums, target);
+	        System.out.println(result[0] + ", " + result[1]);
+
+   }
+}
