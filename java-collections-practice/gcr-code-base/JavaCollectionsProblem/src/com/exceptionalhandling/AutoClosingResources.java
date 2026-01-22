af85eef3a0c43ca3e101ef4f529bc785b74b8165
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-23 01:21:45 +0530

    [Riya]: Added for Auto Closing Resources

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/exceptionalhandling/AutoClosingResources.java
@@ -0,0 +1,25 @@
+package com.exceptionalhandling;
+
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AutoClosingResources {
+  public static void main(String []args) {
+	  try {
+	  BufferedReader rd = new BufferedReader(new FileReader("C:\\Users\\riya6\\OneDrive\\Desktop\\info.txt"));
+	  String line;
+	  while ((line = rd.readLine()) != null) {
+          System.out.println(line);
+      }
+
+      rd.close();
+      }catch(IOException e) {
+          System.out.println("Error Reading File");
+      
+  }
+  
+   }
+}
