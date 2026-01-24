b0f394843c27a27ac2df42adfd53278a629e9d77
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-24 11:24:34 +0530

    [Riya] : Added for the current changes

--- java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/exceptionalhandling/AutoClosingResources.java
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/exceptionalhandling/AutoClosingResources.java
@@ -8,14 +8,14 @@ import java.io.IOException;
 
 public class AutoClosingResources {
   public static void main(String []args) {
-	  try {
-	  BufferedReader rd = new BufferedReader(new FileReader("C:\\Users\\riya6\\OneDrive\\Desktop\\info.txt"));
+	  try( BufferedReader rd = new BufferedReader(new FileReader("C:\\Users\\riya6\\OneDrive\\Desktop\\info.txt"))) {
+	 
 	  String line;
 	  while ((line = rd.readLine()) != null) {
           System.out.println(line);
       }
 
-      rd.close();
+
       }catch(IOException e) {
           System.out.println("Error Reading File");
       
