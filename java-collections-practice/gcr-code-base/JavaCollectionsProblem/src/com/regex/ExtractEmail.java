00dd37efec6308e097986869418216d0a94e1abe
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-24 17:39:25 +0530

    [Riya] : Added for Extracting Email

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/regex/ExtractEmail.java
@@ -0,0 +1,24 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractEmail {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Enter the text:");
+        String text = kb.nextLine();   // read full line
+
+        String regex = "[a-zA-Z0-9._]+@[a-zA-Z0-9._]+\\.[a-zA-Z]{2,}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+        kb.close();
+    }
+}
