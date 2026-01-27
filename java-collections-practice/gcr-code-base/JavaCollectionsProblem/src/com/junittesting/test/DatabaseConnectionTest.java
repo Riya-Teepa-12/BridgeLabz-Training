3e220bad0ba6ce8d8fad41cc26300b3308a422fe
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-27 13:32:57 +0530

    [Riya] : Added for the Data Base Connection

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/junittesting/test/DatabaseConnectionTest.java
@@ -0,0 +1,35 @@
+package com.junittesting.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.junittesting.com.DatabaseConnection;
+
+public class DatabaseConnectionTest {
+ 
+	 private DatabaseConnection db;
+
+	    @BeforeEach
+	    void setUp() {
+	        db = new DatabaseConnection();
+	        db.connect();
+	    }
+
+	    @AfterEach
+	    void tearDown() {
+	        db.disconnect();
+	    }
+
+	    @Test
+	    void testConnectionEstablished() {
+	        assertTrue(db.isConnected());
+	    }
+
+	    @Test
+	    void testConnectionClosed() {
+	        db.disconnect();
+	        assertFalse(db.isConnected());
+	    }
+}
