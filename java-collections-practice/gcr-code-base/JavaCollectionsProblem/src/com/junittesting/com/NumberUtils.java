502b50abee036613ca99cc99afd03728c2c5e9ce
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-27 13:28:22 +0530

    [Riya] : Added for the Number Utils

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/junittesting/com/NumberUtils.java
@@ -0,0 +1,8 @@
+package com.junittesting.com;
+
+public class NumberUtils {
+	 // Returns true if number is even
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
