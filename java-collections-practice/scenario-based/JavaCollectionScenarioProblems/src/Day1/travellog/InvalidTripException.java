7a5f729e1c4dea121093d897f629c5c52c981e23
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-27 16:13:25 +0530

    [Riya] : Added for the Travel Log

---
+++ java-collections-practice/scenario-based/JavaCollectionScenarioProblems/src/Day1/travellog/InvalidTripException.java
@@ -0,0 +1,9 @@
+package Day1.travellog;
+
+public class InvalidTripException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTripException(String msg) {
+		super(msg);
+	}
+}
