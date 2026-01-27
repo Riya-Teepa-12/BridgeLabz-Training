ec857c006f12d9cefdaa22b677cb4360fd092677
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-27 13:28:42 +0530

    [Riya] : Added for the Password Validator

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/junittesting/com/PasswordValidator.java
@@ -0,0 +1,24 @@
+package com.junittesting.com;
+
+public class PasswordValidator {
+	 // Returns true if password is strong
+    public static boolean isValid(String password) {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+
+        boolean hasUppercase = false;
+        boolean hasDigit = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                hasUppercase = true;
+            }
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+        }
+
+        return hasUppercase && hasDigit;
+    }
+}
