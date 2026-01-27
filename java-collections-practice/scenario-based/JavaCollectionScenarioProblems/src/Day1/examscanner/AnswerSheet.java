a542e7522ab0f1d6be5612dfea0dda21d86c8a32
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-27 16:12:23 +0530

    [Riya] : Added for the Exam Scanner

---
+++ java-collections-practice/scenario-based/JavaCollectionScenarioProblems/src/Day1/examscanner/AnswerSheet.java
@@ -0,0 +1,6 @@
+package Day1.examscanner;
+
+public interface AnswerSheet {
+	 String getSubjectName();
+	    String[] getCorrectAnswers();
+}
---
+++ java-collections-practice/scenario-based/JavaCollectionScenarioProblems/src/Day1/examscanner/UseExamScanner.java
@@ -0,0 +1,22 @@
+package Day1.examscanner;
+
+public class UseExamScanner {
+	 public static void main(String[] args) {
+	        // choose subject
+	        MathAnswerSheet mathKey = new MathAnswerSheet();
+	        ExamScanner<MathAnswerSheet> mathScanner = new ExamScanner<>(mathKey);
+
+	        // change to your actual file path
+	        String csvFile = "src/samplefiles/mathanswer.csv";
+
+	        System.out.println("Processing Math exam answers...");
+	        mathScanner.processFile(csvFile);
+	        mathScanner.showResults();
+
+	        // example for another subject
+	         ScienceAnswerSheet scienceKey = new ScienceAnswerSheet();
+	         ExamScanner<ScienceAnswerSheet> scienceScanner = new ExamScanner<>(scienceKey);
+	         scienceScanner.processFile("src/samplefiles/scienceanswers.csv");
+	         scienceScanner.showResults();
+	    }
+}
