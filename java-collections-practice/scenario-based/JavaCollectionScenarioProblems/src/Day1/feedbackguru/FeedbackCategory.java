2cd039d7b73708fd4a7f6d45959ced180c0f0305
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-27 16:13:00 +0530

    [Riya] : Added for the Feed Back Guru

---
+++ java-collections-practice/scenario-based/JavaCollectionScenarioProblems/src/Day1/feedbackguru/FeedbackCategory.java
@@ -0,0 +1,5 @@
+package Day1.feedbackguru;
+
+public enum FeedbackCategory {
+	POSITIVE, NEUTRAL, NEGATIVE
+}
---
+++ java-collections-practice/scenario-based/JavaCollectionScenarioProblems/src/Day1/feedbackguru/UseFeedbackMain.java
@@ -0,0 +1,18 @@
+package Day1.feedbackguru;
+
+public class UseFeedbackMain {
+	public static void main(String[] args) {
+		// create analyzer (String as text)
+		FeedbackAnalyzer<String> guru = new FeedbackAnalyzer<>();
+
+		// folder path
+		String feedbackFolder = "src/samplefiles/feedbackfiles";
+
+		System.out.println("Analyzing feedback files...\n");
+		guru.analyzeFolder(feedbackFolder);
+
+		guru.printSummary();
+
+		System.out.println("Total feedback entries processed: " + guru.getTotalFeedback());
+	}
+}
