b33d5608818871c01e609c600a4bccb5f3223de1
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-01-27 13:32:35 +0530

    [Riya] : Added for the Calculator Test

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/junittesting/test/CalculatorTest.java
@@ -0,0 +1,45 @@
+package com.junittesting.test;
+
+import com.junittesting.com.Calculator;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        calculator.divide(10, 0);
+    }
+}
