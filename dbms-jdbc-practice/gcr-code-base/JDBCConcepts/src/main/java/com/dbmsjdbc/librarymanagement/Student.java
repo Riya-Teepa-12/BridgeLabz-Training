bb1fafa0adef6536cd0905e6387e6c3b5d38ab3e
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-16 12:32:44 +0530

    [Riya] : Add for the implementing library managemet system

---
+++ dbms-jdbc-practice/gcr-code-base/JDBCConcepts/src/main/java/com/dbmsjdbc/librarymanagement/Student.java
@@ -0,0 +1,12 @@
+package com.dbmsjdbc.librarymanagement;
+
+public class Student {
+	private int studentId;
+    private String studentName;
+
+    public Student(int studentId, String studentName) {
+
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+}
