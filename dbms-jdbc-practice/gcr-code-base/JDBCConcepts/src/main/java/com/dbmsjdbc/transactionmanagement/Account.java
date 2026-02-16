045041920bdcef013aeaa46f498bfd4f240e9a2d
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-16 12:33:03 +0530

    [Riya] : Add for the implementing transacction management system

---
+++ dbms-jdbc-practice/gcr-code-base/JDBCConcepts/src/main/java/com/dbmsjdbc/transactionmanagement/Account.java
@@ -0,0 +1,30 @@
+package com.dbmsjdbc.transactionmanagement;
+
+public class Account {
+	 private int accountId;
+	    private String holderName;
+	    private double balance;
+
+	    // default constructor
+	    public Account() {
+	    }
+
+	    // parameterized constructor
+	    public Account(int accountId, String holderName, double balance) {
+	        this.accountId = accountId;
+	        this.holderName = holderName;
+	        this.balance = balance;
+	    }
+
+	    public int getAccountId() {
+	        return accountId;
+	    }
+
+	    public String getHolderName() {
+	        return holderName;
+	    }
+
+	    public double getBalance() {
+	        return balance;
+	    }
+}
